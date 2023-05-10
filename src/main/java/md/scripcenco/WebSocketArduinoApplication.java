bdb8bf61efcf75cbda6fc0a1bea360ecb99a40e4
Scripcenco Alexandru <scripcenco.alexandru@mib.utm.md>
2023-05-10 10:19:36 +0300

    first commit

---
+++ src/main/java/md/scripcenco/WebSocketArduinoApplication.java
@@ -0,0 +1,13 @@
+package md.scripcenco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketArduinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketArduinoApplication.class, args);
+	}
+
+}
